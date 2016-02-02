04f2e034ebd401c9ef64b6374734026f784c65d8
spencermehta <spencemehta@gmail.com>
2016-02-02 20:07:43 +0000

    Implemented retest feature after unexpected results
    Warning and action limits set, retesting occurs if download speed is
    below warning limit. Results are recorded to csv anyway but tweet is
    only sent if the retest is also below warning limit.

--- speedtest_bot.py
+++ speedtest_bot.py
@@ -8,9 +8,53 @@ import sched
 import twitter
 
 
-s = sched.scheduler(time.time, time.sleep)
+#Download speed below which you are tweeted with warning of slow connection
+warning_limit = 60
+#Download speed below which your ISP is tweeted as well as you about slow connection
+action_limit = 50
 
 def test(sc):
+        #Run speedtest-cli
+        print("Running speedtest-cli")
+        a = os.popen("python /path/to/speedtest_cli.py --simple --share").read()
+        print("Ran speedtest-cli")
+        
+        lines = a.split("\n")
+        print(a)
+        ts = time.time()
+        date = datetime.datetime.fromtimestamp(ts).strftime("%Y-%m-%d %H:%M:%S")
+        
+        #Set speeds to 0 if speedtest-cli couldn't connect
+        if "Cannot" in a:
+                p = 100
+                d = 0
+                u = 0
+                
+        #Extract values for ping, download and upload
+        else:
+                p = lines[0][6:11]
+                d = lines[1][10:14]
+                u = lines[2][8:12]
+                img = lines[3][15:]
+        print(date, p, d, u)
+        
+        #Save the data to file for local network plotting
+        out_file = open('/path/to/webserver/data.csv', 'a')
+        writer = csv.writer(out_file)
+        writer.writerow((date,p,d,u))
+        out_file.close()
+        
+        d = float(d)
+        
+        if d < warning_limit:
+                print("UNEXPECTED RESULT - re-running test")
+                retest(sc)
+        else:
+                sc.enter(1800, 1, test, (sc,))
+
+
+
+def retest(sc):
         #Run speedtest-cli
         print("Running speedtest-cli")
         a = os.popen("python /path/to/speedtest_cli.py --simple --share").read()
@@ -47,7 +91,7 @@ def test(sc):
         TOKEN_KEY=""
         CON_SEC=""
         CON_SEC_KEY=""
- 
+        
         my_auth = twitter.OAuth(TOKEN,TOKEN_KEY,CON_SEC,CON_SEC_KEY)
         twit = twitter.Twitter(auth=my_auth)
         
@@ -55,25 +99,28 @@ def test(sc):
         u = float(u)
         
         #Tweet ISP if significantly below what you pay for
-        if d < 50:
-                print("Trying to tweet ISP")
+        if d < action_limit:
+                print("Download speed below action limit. Tweeting ISP")
                 try:
                         tweet="@ISP @me My internet has fallen significantly below what I pay for, now at " + str(d) + "down\\" + str(u) + "up - " + str(img)
                         twit.statuses.update(status=tweet)
-                except:
-                        print("Error tweeting")
+                except Exception as e:
+                        print("Error tweeting:", e)
                         
         
-        #Tweet you if slightly below what you pay for                
-        elif d < 60:
-                print("Trying to tweet you")
+        #Tweet you if slightly below what you pay for
+        elif d < warning_limit:
+                print("Download speed below warning limit. Tweeting you")
                 try:
                         tweet="@me Your internet speed has fallen below acceptable levels, currently at " + str(d) + "down\\" + str(u) + "up - " + str(img)
                         twit.statuses.update(status=tweet)
-                except:
-                        print("Error tweeting")
+                except Exception as e:
+                        print("Error tweeting:", e)
         
         sc.enter(1800, 1, test, (sc,))
 
+
+
+s = sched.scheduler(time.time, time.sleep)
 s.enter(1800, 1, test, (s,))
 s.run()
