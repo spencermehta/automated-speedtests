e33dbaf69149f80e095ad959d66a343cfaab3346
spencer <spencer@spencermehta.co.uk>
2016-02-08 15:22:13 +0000

    Separated stuff out into separate functions

--- automated-speedtests.py
+++ automated-speedtests.py
@@ -7,99 +7,112 @@ import time
 import twitter
 import config
 
-def test():
-    passed_test = False
-    counter = 0
-        
-    while not passed_test and counter < 2:
-        counter += 1
-        if counter == 2:
-            print("Result below warning limit. Re-running test")
-        #Run speedtest-cli
-        print("Running speedtest-cli")
-        a = os.popen("python " + config.speedtest_cli_path + " --simple --share").read()
-        print("Ran speedtest-cli")
-                
-        lines = a.split("\n")
-        print(a)
+
+
+print("""
+      
+-----------------------------
+Starting automated-speedtests
+-----------------------------
+""")
+
+
+
+ping = False
+down = False
+up = False
+img = False
+tests = 0
+
+
+
+def speedTest(ping, down, up, img, tests):
+        print("\nConducting speed test...")
+        speedTestOutput = os.popen("python " + config.speedtest_cli_path + " --simple --share").read()
+        print("Speed test complete")
+
+        lines = speedTestOutput.split("\n")
         ts = time.time()
         date = datetime.datetime.fromtimestamp(ts).strftime("%Y-%m-%d %H:%M:%S")
-                
+        
+        print("\n------------------------------------------------------")
+        print(date + "\n" + speedTestOutput + "------------------------------------------------------\n")
         #Set speeds to 0 if speedtest-cli couldn't connect
-        if "Cannot" in a:
-            p = 100
-            d = 0
-            u = 0
-                        
+        if "Cannot" in speedTestOutput:
+            ping = 100
+            down = 0
+            up = 0
+            img = "noimg"
+
         #Extract values for ping, download and upload
         else:
-            p = lines[0].split(' ')[1]
-            d = lines[1].split(' ')[1]
-            u = lines[2].split(' ')[1]
+            ping = lines[0].split(' ')[1]
+            down = lines[1].split(' ')[1]
+            up = lines[2].split(' ')[1]
             img = lines[3].split(' ')[2]
 
-        print(date, p, d, u)
-                
+
         #Save the data to file for local network plotting
-        out_file = open(config.webserver_path + "/speedresults.csv", 'a')
-        writer = csv.writer(out_file)
-        writer.writerow((date,p,d,u))
-        out_file.close()
-                
-        d = float(d)
-                
-        if d >= config.warning_limit:
-            passed_test = True
-                        
-    if not passed_test and counter == 2:
-                     
+        fileSpeedResults = open(config.webserver_path + "/speedresults.csv", 'a')
+        writer = csv.writer(fileSpeedResults)
+        writer.writerow((date, ping, down, up))
+        fileSpeedResults.close()
+
+        ping = float(ping)
+        down = float(down)
+        up = float(up)
+
+        tests += 1
+
+        return(ping, down, up, img, tests)        
+
+
+
+def tweet(tweetcontent, ping, down, up, img):
+        #Twitter API connection
         my_auth = twitter.OAuth(config.TOKEN, config.TOKEN_KEY, config.CON_SEC, config.CON_SEC_KEY)
         twit = twitter.Twitter(auth=my_auth)
-        #Tweet ISP if significantly below what you pay fo
-        if d < config.action_limit:
-            print("Download speed below action limit. Tweeting ISP")
-                        
-            tweet = config.action_tweet
-            if '%p' in tweet:
-                tweet = tweet.replace('%p', str(p))
-            if '%d' in tweet:
-                tweet = tweet.replace('%d', str(d))
-            if '%u' in tweet:
-                tweet = tweet.replace('%u', str(u))
-            if '%img' in tweet:
-                tweet = tweet.replace('%img', str(img))
 
-            try:
-                twit.statuses.update(status=tweet)
-                                              
-            except Exception as e:
-                print("Error tweeting:", e)
-                                
-                
-        #Tweet you if slightly below what you pay for
-        elif d < config.warning_limit:
-            print("Download speed below warning limit. Tweeting you")
-                             
-            tweet = config.warning_tweet
-            if '%p' in tweet:
-                tweet = tweet.replace('%p', str(p))
-            if '%d' in tweet:
-                tweet = tweet.replace('%d', str(d))
-            if '%u' in tweet:
-                tweet = tweet.replace('%u', str(u))
-            if '%img' in tweet:
-                tweet = tweet.replace('%img', str(img))
+        print("Tweeting...")
 
-            try:
-                twit.statuses.update(status=tweet)
-                                              
-            except Exception as e:
-                print("Error tweeting:", e)
-                        
+        tweet = tweetcontent
+        if '%p' in tweet:
+            tweet = tweet.replace('%p', str(ping))
+        if '%d' in tweet:
+            tweet = tweet.replace('%d', str(down))
+        if '%u' in tweet:
+            tweet = tweet.replace('%u', str(up))
+        if '%img' in tweet:
+            tweet = tweet.replace('%img', str(img))
+
+        try:
+            twit.statuses.update(status=tweet)
+            print(tweet)
+            print("Tweeted successfully")
+
+        except Exception as e:
+            print("Error tweeting:", e)
+
+
+
+def sleep(start_time):
+    sleep_time = max(config.interval - (time.time() - start_time), 0)
+    print("\nSleeping for ", round(sleep_time, 0), " seconds")
+    time.sleep(sleep_time)
+
+
+
 
 while True:
     start_time = time.time()
-    test()
-    sleep_time = max(config.interval - (time.time() - start_time), 0)
-    print("Sleeping for ", round(sleep_time, 0), " seconds")
-    time.sleep(sleep_time)
+        
+    ping, down, up, img, tests = speedTest(ping, down, up, img, tests)
+    
+    if down < config.action_limit and tests > 1:
+        tweet(config.action_tweet, ping, down, up, img)
+        sleep(start_time)
+    elif down < config.warning_limit and tests > 1:
+        tweet(config.warning_tweet, ping, down, up, img)
+        sleep(start_time)
+    else:
+        print("***Unexpected result. Repeating speed test***")
